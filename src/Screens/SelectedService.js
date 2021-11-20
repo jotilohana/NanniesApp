0410f7b7452f982f4ccb5a1fe8742b505910a703
jotilohana <jklohana208@gmail.com>
2021-11-20 22:26:44 +0500

    ServiceScreen in progrrs

--- src/Screens/SelectedService.js
+++ src/Screens/SelectedService.js
@@ -4,7 +4,7 @@ import {View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
 
 const SelectedService =()=>{
     return(
-         <View style={{ alignItems: 'center', flex:1}}>
+        <View style={{ alignItems: 'center', flex:1}}>
             <View style={{marginTop:50, flexDirection:'row',alignItems: 'center',alignSelf:"center" }}>
                 <View>
                 <TouchableOpacity
@@ -14,6 +14,7 @@ const SelectedService =()=>{
                 <Text style={styles.buttonText}>InProgress</Text>
                 </TouchableOpacity>
                 </View>
+
                 <View>
                     <TouchableOpacity
                 style={styles.button}
@@ -22,6 +23,7 @@ const SelectedService =()=>{
                 <Text style={styles.buttonText}>Finished</Text>
                  </TouchableOpacity>
                 </View>
+
                 <View>
                 <TouchableOpacity
                 style={styles.button}
@@ -30,9 +32,10 @@ const SelectedService =()=>{
                 <Text style={styles.buttonText}>Canceled</Text>
                 </TouchableOpacity>
                 </View>
+
             </View>
 
-                {/* Card View */}
+            {/* Card View */}
             <View style={styles.CardView}>
                 <View>
                     <Image
@@ -41,20 +44,24 @@ const SelectedService =()=>{
                      />
                 </View>
                 <View>
-                    <View style={{flexDirection:"row", margin:15}}>
+                    <View style={styles.headerView}>
                     <Text style={styles.cardText}>Service Name</Text>
                     <Image
                     style={styles.Dots}
                     source={require('../Assets/Dots.png')}
                      />
                     </View>
+                    <View style={styles.bottom}>
+                    <Text style={styles.bottomTextTime}><Text style={{color:'#F69153',fontSize:22,fontWeight:"bold"}}>10:00</Text>PM to <Text style={{color:'#F69153',fontSize:22,fontWeight:"bold"}}>12:00</Text>PM</Text>
+                    <View style={{alignSelf:"center",width:'80%', backgroundColor:'#fff', height:1}}></View>
+                    <Text style={styles.bottomTextDate}><Text style={{color:'#F69153',fontSize:22,fontWeight:"bold"}}>20</Text>-Nov-2021 to <Text style={{color:'#F69153',fontSize:22,fontWeight:"bold"}}>22</Text>-Nov-21</Text>
+                    </View>
                 </View>
-            </View>
-         </View>
-                  
-        )
-}
-const styles=StyleSheet.create({
+            </View>   
+            </View> 
+        );
+};
+const styles= StyleSheet.create({
     button:{
         backgroundColor:"#03204c",
         margin:15,
@@ -71,22 +78,29 @@ const styles=StyleSheet.create({
     //Card
 
     CardView:{
-        flexDirection:"row",
+        flexDirection:'row',
         borderwidth:4,
         height:200,
-        backgroundColor:"#FFFFFF",
+        backgroundColor:"white",
         width:'90%',
         borderRadius:25,
         marginTop:25,
-        padding:20
     },
     CardImage:{
         width:100,
-        height:160,
-        borderRadius:26
-        // borderTopLeftRadius:25,
-        // borderBottomLeftRadius:25
+        height:200,
+        borderTopLeftRadius:25,
+        borderBottomLeftRadius:25
     },
+    headerView:{
+        elevation: 5,
+        flexDirection:"row",
+        backgroundColor:"white",
+        height:50,
+        width:225,
+        borderTopRightRadius:10,
+        padding:10
+      },
     cardText:{
         fontSize:25,
         marginRight:20,
@@ -96,10 +110,27 @@ const styles=StyleSheet.create({
         height:20,
         width:20,
         marginTop:10
-
+    },
+    bottom:{
+        backgroundColor:"#03204c",
+        padding:5,
+        borderBottomRightRadius:20,
+        height:150,
+    },
+    bottomTextTime:{
+        color:"#FFFFFF",
+        fontSize:16,
+        marginTop:70,
+        font:'Roboto',
+        textAlign:'center'
+    },
+    bottomTextDate:{
+        color:"#FFFFFF",
+        fontSize:16,
+        font:'Roboto',
+        textAlign:'center',
     }
-
-})
+});
 
 export default SelectedService;
 
---
+++ src/Screens/particularScreenDetails.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+const Details=()=>{
+    return(
+        <View style={{flex:1}}>
+        
+        <View style={styles.FirstView}>
+
+        </View>
+               
+        </View>
+    )
+}
+
+const styles=StyleSheet.create({
+    FirstView:{
+        backgroundColor:'White',
+        
+    }
+})
